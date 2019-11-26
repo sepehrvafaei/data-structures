d0e9daa9e82ecca1d59ad028adb8c1bb54cdc4d1
sepehrvafaei <54388517+sepehrvafaei@users.noreply.github.com>
2019-11-25 21:33:12 -0800

    Update hash.cpp

--- hash.cpp
+++ hash.cpp
@@ -57,7 +57,6 @@ void hash::PrintTable() {
 		std::cout << "---------------------" << std::endl;
 	}
 }
-
 int hash::Hash(std::string key) {
 	int hash = 0;
 	int index;
@@ -67,3 +66,69 @@ int hash::Hash(std::string key) {
 	index = hash % tableSize;
 	return index;
 }
+void hash:: PrintItemsOfIndex(int index) {
+	item* Ptr = HashTable[index];
+	if (Ptr->name == "empty") {
+		std::cout << "index " << index << " is empty.";
+	}
+	else {
+		std::cout << "index " << index << " contains the following items:\n";
+		while (Ptr != nullptr) {
+			std::cout << Ptr->name << std::endl;
+			std::cout << Ptr->drink << std::endl;
+			Ptr = Ptr->next;
+		}
+	}
+}void hash::FindDrink(std::string name) {
+	int index = Hash(name);
+	item* Ptr = HashTable[index];
+	while (Ptr!=nullptr) {
+		if (Ptr->name == name) {
+			std::cout << name << "'s " << "drink is " << Ptr->drink << std::endl;
+			break;
+		}
+		Ptr = Ptr->next;
+	}
+	if (Ptr == nullptr) {
+		std::cout << name << " is not in the Hash Table.\n";
+	}
+}
+void hash::RemoveItem(std::string name) {
+	item* delPtr;
+	item* P1;
+	item* P2;
+	int index = Hash(name);
+	if (HashTable[index]->name == "empty") {
+		std::cout << name << " is not in Hash Table.\n";
+	}
+	else if(HashTable[index]->name==name && HashTable[index]->next==nullptr) {
+		HashTable[index]->name = "empty";
+		HashTable[index]->drink = "empty";
+	}
+	else if (HashTable[index]->name == name) {
+		delPtr = HashTable[index];
+		HashTable[index] = HashTable[index]->next;
+		delete delPtr;
+		std::cout << name << " is removed from Hash Table.\n";
+	}
+	else {
+		P1 = HashTable[index]->next;
+		P2 = HashTable[index];
+		while (P1 != nullptr && P1->name != name) {
+			P2 = P1;
+			P1 = P1->next;
+		}
+		if(P1==nullptr){
+			std::cout << name << " is not in Hash Table.\n";
+		}
+		else {
+			delPtr = P1;
+			P2->next = P1->next;
+			delete delPtr;
+		}
+	}
+
+}
+
+
+
