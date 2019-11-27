51af8da7a92e83ee370d796f0eb0b6fba59c4a6c
sepehrvafaei <54388517+sepehrvafaei@users.noreply.github.com>
2019-11-26 17:25:10 -0800

    Update BST.h

--- BST.h
+++ BST.h
@@ -7,8 +7,13 @@ private:
 		node* right;
 	};
 	node* root;
+	void AddLeafPrivate(int key,node* Ptr);
+	void PrintInOrderPrivate(node* Ptr);
 public:
 	BST();
 	node* CreatLeaf(int key);
+	void AddLeaf(int key);
+	void PrintInOrder();
 
 };
+
