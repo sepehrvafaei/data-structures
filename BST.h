5a9ba1636a03c13b8504c703bb1ca4f7f2d5cfd4
sepehrvafaei <54388517+sepehrvafaei@users.noreply.github.com>
2019-12-22 00:09:49 -0800

    Update BST.h

--- BST.h
+++ BST.h
@@ -9,11 +9,22 @@ private:
 	node* root;
 	void AddLeafPrivate(int key,node* Ptr);
 	void PrintInOrderPrivate(node* Ptr);
+	node* ReturnNodePrivate(int key, node* ptr);
+	int FindSmallestPrivate(node* ptr);
+	void RemoveNodePrivate(int key, node* parent);
+	void RemoveRootMatch();
+	void RemoveMatch(node* parent,node* match,bool left);
+
 public:
 	BST();
 	node* CreatLeaf(int key);
 	void AddLeaf(int key);
 	void PrintInOrder();
+	node* ReturnNode(int key);
+	int ReturnRootKey();
+	void PrintChildren(int key);
+	int FindSmallest();
+	void PrintHierarchy();
+	void RemoveNode(int key);
 
 };
-
